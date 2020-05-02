affbe5ce8c83a180a516d7325d33a19e99cc3fd8
roxanahefferon <roxana.hefferon@gmail.com>
2020-05-02 22:26:10 +0000

    flask app

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-from flask import flask
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hello world... again'
+
+if __name__ == '__main__':
+    host=os.environ.get('IP'),
+    port=(os.environ.get('PORT')),
+    debug=(True)
