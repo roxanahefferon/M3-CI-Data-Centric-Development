d4b7c207514008fdc5939bb22f086244b8cbb094
roxanahefferon <roxana.hefferon@gmail.com>
2020-05-02 22:53:59 +0000

    templates, index.html

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
     return 'hello world... again'
 
+
 if __name__ == '__main__':
-    host=os.environ.get('IP'),
-    port=(os.environ.get('PORT')),
-    debug=(True)
+    host = os.environ.get('IP'),
+    port = (os.environ.get('PORT')),
+    debug = (True)
